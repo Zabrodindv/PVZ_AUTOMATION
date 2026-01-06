ba2b65c28739a72f08bf48ea5128925d500c65eb
Denis <denis@MacBook-Pro-Denis-2.local>
2026-01-06 16:52:03 +0300

    Force IPv4 for requests on Raspberry Pi

--- encashment/telegram.py
+++ encashment/telegram.py
@@ -4,7 +4,15 @@
 
 import os
 import sys
+import socket
 import subprocess
+
+# Принудительно использовать IPv4 для requests (IPv6 не работает на Raspberry Pi)
+_original_getaddrinfo = socket.getaddrinfo
+def _getaddrinfo_ipv4(host, port, family=0, type=0, proto=0, flags=0):
+    return _original_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
+socket.getaddrinfo = _getaddrinfo_ipv4
+
 import requests
 import pandas as pd
 import pytz
